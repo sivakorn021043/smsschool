34412e270c1a4448cf07415aa83356d765b28150
ศิวกร คันธทรัพย์ <yok@MacBook-Air.local>
2025-11-23 21:29:33 +0700

    fix delete image route and news editor

--- app/api/news/delete-image/[id]/route.ts
+++ app/api/news/delete-image/[id]/route.ts
@@ -3,10 +3,10 @@ import { supabaseServer } from "@/lib/supabaseServer";
 
 export async function POST(
   req: Request,
-  { params }: { params: { filename: string } }
+  { params }: { params: { id: string } }
 ) {
   try {
-    const filename = params.filename;
+    const filename = params.id;
     const bucket = process.env.SUPABASE_BUCKET!;
 
     const { error } = await supabaseServer.storage
